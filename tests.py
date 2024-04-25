a58473e3efeec32a1d8c6575a6350fe43d966af0
Stepan Kazakov <103280625+StepanKazakov@users.noreply.github.com>
2024-04-25 19:09:29 +0200

    убрал все assert перед шагами, assert теперь только проверяют результат теста

--- tests.py
+++ tests.py
@@ -15,7 +15,6 @@ class TestBooksCollector:
         my_books = collector.get_books_genre()
         assert len(my_books) == 1
         assert name in my_books
-        assert my_books[name] == ''
 
     def test_add_new_book_check_boundary_values_negative(self, collector):
         collector.add_new_book('')                   # Empty name
@@ -40,37 +39,33 @@ class TestBooksCollector:
 
     def test_set_book_genre_change_genre_return_last_value(self, collector):
         collector.add_new_book('1984')
-        collector.set_book_genre('1984', 'Фантастика')
+        collector.set_book_genre('1984', 'Фантастика')    # set initial genre
+        collector.set_book_genre('1984', 'Комедии')       # change genre
         books_genres = collector.get_books_genre()
-        assert books_genres['1984'] == 'Фантастика'
-        collector.set_book_genre('1984', 'Комедии')
-        assert books_genres['1984'] == 'Комедии'
-        assert books_genres['1984'] != 'Фантастика'
+        assert books_genres['1984'] == 'Комедии'          # last value saved
+        assert books_genres['1984'] != 'Фантастика'       # initial value erased
 
     def test_get_books_with_specific_genre_fiction_only(self, collector):
-        assert len(collector.get_books_genre()) == 0
         collector.add_new_book('Star Wars')
         collector.add_new_book('Sherlock Holmes')
         collector.set_book_genre('Star Wars', 'Фантастика')
         collector.set_book_genre('Sherlock Holmes', 'Детективы')
         fiction = collector.get_books_with_specific_genre('Фантастика')
-        assert len(collector.get_books_genre()) == 2
-        assert len(fiction) == 1
-        assert 'Star Wars' in fiction
+        assert len(collector.get_books_genre()) == 2       # total books - 2
+        assert len(fiction) == 1                           # fiction books - 1
+        assert 'Star Wars' in fiction                      # fiction book get correctly
 
     def test_get_books_for_children_exclude_age_restricted_genre(self, collector):
-        assert len(collector.get_books_genre()) == 0
         collector.add_new_book('Metro 2033')
         collector.set_book_genre('Metro 2033', 'Ужасы')
         collector.add_new_book('Alice')
         collector.set_book_genre('Alice', 'Мультфильмы')
         children_books = collector.get_books_for_children()
-        assert len(collector.get_books_genre()) == 2
-        assert len(children_books) == 1
-        assert 'Metro 2033' not in children_books
+        assert len(collector.get_books_genre()) == 2        # total books - 2
+        assert len(children_books) == 1                     # books for children - 1
+        assert 'Metro 2033' not in children_books           # horror not for children
 
     def test_add_book_in_favorites_check_and_remove(self, collector):
-        assert collector.get_list_of_favorites_books() == []
         collector.add_new_book('Metro 2035')
         collector.add_book_in_favorites('Metro 2035')
         assert 'Metro 2035' in collector.get_list_of_favorites_books()
@@ -78,9 +73,7 @@ class TestBooksCollector:
         assert 'Metro 2035' not in collector.get_list_of_favorites_books()
 
     def test_add_book_in_favorites_double_add_list_of_favorites_return_one(self, collector):
-        assert collector.get_list_of_favorites_books() == []
         collector.add_new_book('Lord of the ring')
-        collector.add_book_in_favorites('Lord of the ring')
-        collector.set_book_genre('Lord of the ring', 'Фантастика')
-        collector.add_book_in_favorites('Lord of the ring')
-        assert len(collector.get_list_of_favorites_books()) == 1
+        collector.add_book_in_favorites('Lord of the ring')           # add book in favorites
+        collector.add_book_in_favorites('Lord of the ring')           # add same book in favorites again
+        assert len(collector.get_list_of_favorites_books()) == 1      # favorites not contain doubles
